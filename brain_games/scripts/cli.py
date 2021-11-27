0dc85ee43af36637f728b5bfa9cab67d0b4c1283
Pavel Vasilev <vasilievpg@gmail.com>
2021-11-27 09:35:02 +0300

    Поправил оформление

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -17,19 +17,22 @@ def congratulations(name):
 
 
 def incorrect_answer(answer, correct_answer, name):
-    print(f"'{answer}' is wrong answer ;(. ", end='')
-    print(f"Correct answer was '{correct_answer}'.")
+    print(f"'{answer}' is wrong answer ;(. "
+          f"Correct answer was '{correct_answer}'.")
     print(f"Let's try again, {name}!")
 
 
 def compare_answer(func_question, func_correct_answer, text_conditions):
     name = welcome_user()
     conditions(text_conditions)
+
     for i in range(3):
         question = func_question()
         correct_answer = func_correct_answer(question)
+
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
+
         if answer == correct_answer:
             print('Correct!')
         else:
