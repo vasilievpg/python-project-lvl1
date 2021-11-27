a7bb0a993ccb3e38488a8252b8dbabfaafe40efb
Pavel Vasilev <vasilievpg@gmail.com>
2021-11-27 09:14:23 +0300

    Вынес все по максимуму в cli.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,26 +2,25 @@
 
 from random import randint
 
-from brain_games.scripts.cli import welcome_user, conditions, compare_answer
-from brain_games.scripts.cli import congratulations
+from brain_games.scripts.cli import conditions, compare_answer
 
 
-def brain_even(name):
-    i = 1
-    while i <= 3:
-        question = randint(1, 100)
-        correct_answer = 'yes' if question % 2 == 0 else 'no'
-        if compare_answer(question, correct_answer, name):
-            i += 1
-        else:
-            return False
-    congratulations(name)
+def func_question():
+    return randint(1, 100)
+
+
+def func_correct_answer(question):
+    return 'yes' if question % 2 == 0 else 'no'
+
+
+def brain_even():
+    text_conditions = ('Answer "yes" if the number is even, '
+                       'otherwise answer "no".')
+    compare_answer(func_question, func_correct_answer, text_conditions)
 
 
 def main():
-    name = welcome_user()
-    conditions('Answer "yes" if the number is even, otherwise answer "no".')
-    brain_even(name)
+    brain_even()
 
 
 if __name__ == '__main__':
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -22,12 +22,18 @@ def incorrect_answer(answer, correct_answer, name):
     print(f"Let's try again, {name}!")
 
 
-def compare_answer(question, correct_answer, name):
-    print(f'Question: {question}')
-    answer = prompt.string('Your answer: ')
-    if answer == correct_answer:
-        print('Correct!')
-        return True
-    else:
-        incorrect_answer(answer, correct_answer, name)
-        return False
+def compare_answer(func_question, func_correct_answer, text_conditions):
+    name = welcome_user()
+    conditions(text_conditions)
+    for i in range(3):
+        question = func_question()
+        correct_answer = func_correct_answer(question)
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print('Correct!')
+        else:
+            incorrect_answer(answer, correct_answer, name)
+            return False
+
+    congratulations(name)
