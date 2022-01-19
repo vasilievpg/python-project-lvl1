1e697d2d7a5860190df75eac20ee47e147609869
Pavel Vasilev <vasilievpg@gmail.com>
2022-01-19 11:15:55 +0300

    Переделал brain_calc.py на использование operator

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+from operator import add, sub, mul
 
 text_conditions = "What is the result of the expression?"
 
@@ -7,14 +8,13 @@ def get_question_and_answer():
     correct_answer = 0
     num_1 = randint(1, 10)
     num_2 = randint(1, 10)
-    operations = ("+", "-", "*")
-    operation = choice(operations)
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+    }
+    operation = choice(list(operations))
     question = f"{num_1} {operation} {num_2}"
-    if operation == "+":
-        correct_answer = num_1 + num_2
-    elif operation == "-":
-        correct_answer = num_1 - num_2
-    elif operation == "*":
-        correct_answer = num_1 * num_2
+    correct_answer = operations[operation](num_1, num_2)
 
     return question, str(correct_answer)
