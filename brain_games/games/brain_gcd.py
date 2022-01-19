c11233e2a91e91c24d1c67c5aa34ac527b9f6f3a
Pavel Vasilev <vasilievpg@gmail.com>
2022-01-19 11:28:02 +0300

    Вынес divider в отдельную функцию

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,15 +3,19 @@ from random import randint
 text_conditions = "What is the result of the expression?"
 
 
+def find_divider(num_1, num_2):
+    divider = num_1 if num_1 <= num_2 else num_2
+    for _ in range(divider):
+        if num_1 % divider == 0 and num_2 % divider == 0:
+            return str(divider)
+        else:
+            divider -= 1
+
+
 def get_question_and_answer():
     num_1 = randint(1, 100)
     num_2 = randint(1, 100)
     question = f"{num_1} {num_2}"
-    divider = num_1 if num_1 <= num_2 else num_2
-    for _ in range(divider):
-        if num_1 % divider == 0 and num_2 % divider == 0:
-            return question, str(divider)
-        else:
-            divider -= 1
+    divider = find_divider(num_1, num_2)
 
-    return question, "1"
+    return question, divider
