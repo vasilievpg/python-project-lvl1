4921cb70a6d7232af8cde486cb3d776c47e5e921
Pavel Vasilev <vasilievpg@gmail.com>
2022-01-22 07:01:31 +0300

    Переименовал функцию на find_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,7 +3,7 @@ from random import randint
 DESCRIPTION = "What is the result of the expression?"
 
 
-def find_divider(num_1, num_2):
+def find_gcd(num_1, num_2):
     divider = num_1 if num_1 <= num_2 else num_2
     for _ in range(divider):
         if num_1 % divider == 0 and num_2 % divider == 0:
@@ -16,6 +16,6 @@ def get_question_and_answer():
     num_1 = randint(1, 100)
     num_2 = randint(1, 100)
     question = f"{num_1} {num_2}"
-    answer = find_divider(num_1, num_2)
+    answer = find_gcd(num_1, num_2)
 
     return question, answer
