16db82015c27cb956bc3166d139c665a1afe1e71
Pavel Vasilev <vasilievpg@gmail.com>
2022-01-26 11:18:43 +0300

    Переделал find_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,16 +3,16 @@ from random import randint
 DESCRIPTION = 'What is the result of the expression?'
 
 
-def gcd(p: int, q: int) -> int:
-    while q != 0:
-        (p, q) = (q, p % q)
-    return p
+def find_gcd(num_1, num_2):
+    while num_2 != 0:
+        (num_1, num_2) = (num_2, num_1 % num_2)
+    return num_1
 
 
 def get_question_and_answer():
     num_1 = randint(1, 100)
     num_2 = randint(1, 100)
     question = f'{num_1} {num_2}'
-    answer = str(gcd(num_1, num_2))
+    answer = str(find_gcd(num_1, num_2))
 
     return question, answer
