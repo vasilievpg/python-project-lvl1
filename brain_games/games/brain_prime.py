4b8a6793b71880da4e1fea39ed0ec2a703498c29
Pavel Vasilev <vasilievpg@gmail.com>
2022-01-26 11:26:17 +0300

    Переделал brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -9,7 +9,8 @@ DESCRIPTION = ('Answer "yes" if given number is prime.'
 def is_prime(n):
     if n < 2:
         return False
-    for number in islice(count(2), int(sqrt(n) - 1)):
+    # for number in islice(count(2), int(sqrt(n) - 1)):
+    for number in range(2, int(sqrt(n) - 1)):
         if not n % number:
             return False
     return True
